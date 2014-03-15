bd625c8e825c68eae2dea3735adf79ae73cf620c
Chris Esplin <christopher.esplin@gmail.com>
2014-03-15 14:31:21 -0600

    Adding star nav.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('islcClientApp')
-  .controller('NavCtrl', function ($scope) {
-
+  .controller('NavCtrl', function ($scope, $window) {
+    $scope.env = $window.envVars;
 });
