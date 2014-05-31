0af49ab39a4c347d55df713b4cb23084425c0307
Chris Esplin <christopher.esplin@gmail.com>
2014-05-31 17:03:58 -0600

    Making the token redirect link a bit smarter.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('islcClientApp')
-  .controller('NavCtrl', function ($scope, $window) {
+  .controller('NavCtrl', function ($scope, $window, $location) {
     $scope.env = $window.envVars;
+    $scope.tokenRedirect = $scope.env.islc + '/admin/oauth/forward?redirect=' + $location.protocol() + '://' + $location.host();
 });
